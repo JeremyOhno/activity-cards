980e217bd5b7bf6bd8199be65604c65b589e803c
Jeremy <jeremy.owen11@gmail.com>
2023-09-14 19:34:09 -0700

    Commit 1.1

---
+++ src/main/java/org/example/carddeck/Card.java
@@ -0,0 +1,10 @@
+package org.example.carddeck;
+
+public class Card {
+    private Suit suit;
+    private FaceValue value;
+
+    public Card(Suit suit, FaceValue faceValue) {
+        this.suit = suit;
+    }
+}
